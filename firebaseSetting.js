c3607aba8393259e9aaee0c18477ab1727093b17
Isenewo Oluwaseyi <isenewoephr2012@gmail.com>
2024-06-24 15:30:40 +0100

    edited gitignore

---
+++ firebaseSetting.js
@@ -0,0 +1,10 @@
+
+const SETTINGS = {
+    apiKey: "AIzaSyBflTqV_h9kSc6wULkBd1BtbKqTniQGGlQ",
+    authDomain: "policy-tracker-lac-kad.firebaseapp.com",
+    projectId: "policy-tracker-lac-kad",
+    storageBucket: "policy-tracker-lac-kad.appspot.com",
+    messagingSenderId: "675419575485",
+    appId: "1:675419575485:web:a744a4adfeed8a633913d4",
+    measurementId: "G-GER16PRXMS"
+}
